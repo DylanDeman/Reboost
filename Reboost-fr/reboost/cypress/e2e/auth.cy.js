da780cda4cf0f1f016e500bdbe047a0fe22d32f3
Dylan De Man <dylan.deman@student.hogent.be>
2024-12-17 20:06:28 +0100

    Toevoegen van login test met Auth0

---
+++ Reboost-fr/reboost/cypress/e2e/auth.cy.js
@@ -0,0 +1,14 @@
+// cypress/integration/auth.spec.js
+
+describe('Auth0', () => {
+  beforeEach(() => {
+    const username = 'test@reboost.be';
+    const password = 'Test123.';
+    
+    cy.loginViaAuth0Ui(username, password);
+  });
+
+  it('Moet inloggen en redirecten naar de homepagina', () => {
+    cy.url().should('equal', 'http://localhost:5173/evenementen');
+  });
+});
--- Reboost-fr/reboost/cypress/support/commands.js
+++ Reboost-fr/reboost/cypress/support/commands.js
@@ -8,6 +8,53 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
+
+Cypress.Commands.add('loginToAuth0', (username, password) => {
+  const log = Cypress.log({
+    displayName: 'AUTH0 LOGIN',
+    message: [`🔐 Inloggen... | ${username}`],
+    autoEnd: false,
+  })
+  log.snapshot('before')
+
+  cy.session(
+    `auth0-${username}`,
+    () => {
+      cy.loginViaAuth0Ui(username, password)
+    },
+    {
+      validate: () => {
+        cy.wrap(localStorage)
+          .invoke('getItem', 'authAccessToken')
+          .should('exist')
+      },
+    }
+  )
+
+  log.snapshot('after')
+  log.end()
+})
+
+
+Cypress.Commands.add('loginViaAuth0Ui', (username, password) => {
+  cy.visit('http://localhost:5173');
+
+  cy.origin(
+    'dev-eu5g8sejk0b6k23l.us.auth0.com',
+    { args: { username, password } },
+    ({ username, password }) => {
+      cy.get('input#username').type(username);
+      cy.get('input#password').type(password, { log: false });
+      cy.contains('button[value=default]', 'Continue').click();
+    }
+  );
+
+  cy.url().should('equal', 'http://localhost:5173/evenementen');
+});
+
+
+
+
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
