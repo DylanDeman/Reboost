0854b4ed34bda7fa12a60997ae11383c6846d0aa
Dylan De Man <dylan.deman@student.hogent.be>
2025-07-30 16:49:15 +0200

    Adds gereedschap type definitions

--- Reboost-ws/src/types/gereedschap.ts
+++ Reboost-ws/src/types/gereedschap.ts
@@ -12,3 +12,40 @@ export interface Gereedschap extends Entity {
     datum: Date;
   }; 
 }
+
+export interface GereedschapCreateInput {
+  naam: string;
+  beschrijving: string;
+  beschikbaar: boolean;
+  verhuurd: boolean;
+  evenementId?: number; 
+}
+
+export interface GereedschapUpdateInput {
+  naam?: string;
+  beschrijving?: string;
+  beschikbaar?: boolean;
+  verhuurd?: boolean;
+  evenementId?: number; 
+}
+
+export interface GereedschapListResponse {
+  gereedschappen: Gereedschap[];
+  totalCount: number;
+}
+export interface GereedschapByIdResponse extends Gereedschap {}
+export interface GereedschapCreateResponse extends Gereedschap {}
+export interface GereedschapUpdateResponse extends Gereedschap {}
+export interface GereedschapDeleteResponse {
+  success: boolean;
+  message: string;
+}
+
+export interface GereedschapEventResponse {
+  gereedschap: Gereedschap;
+  evenement: {
+    id: number;
+    naam: string;
+    datum: Date;
+  };
+}
